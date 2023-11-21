090722915e00261b1cb126735bc1e21072152650
unknown <andrewfandy13@gmail.com>
2023-11-21 23:14:32 +0700

    Refactoring design and javascript

--- app.js
+++ app.js
@@ -1,63 +1,66 @@
-// TODO 
-// REFACTORING INPUTELEMENTS
-
-
 const inputElements = {
-    "userInputDay": document.getElementById("inputDay"),
-    "userInputMonth": document.getElementById("inputMonth"),
-    "userInputYear": document.getElementById("inputYear"),
+  inputDay: {
+    getElement: document.getElementById("inputDay"),
+    maxlength: 2,
+    min: 1,
+    max: () => {
+      const yearValue = inputElements.inputYear.getElement.value;
+      const monthValue = inputElements.inputMonth.getElement.value;
+      return new Date(yearValue, monthValue, 0).getDate();
+    },
+  },
+  inputMonth: {
+    getElement: document.getElementById("inputMonth"),
+    maxlength: 2,
+    min: 1,
+    max: 12,
+  },
+  inputYear: {
+    getElement: document.getElementById("inputYear"),
+    maxlength: 4,
+    min: 1,
+    max: currentDate.getFullYear(),
+  },
 };
-
-
-const getCalendarDay = (year, month) => {
-    return new Date(year, month, 0).getDate();
-}
 
 // Input Modification
 const modifyInputLength = (input, maxlength) => {
-    if(input.value.length > maxlength){
-        input.value = input.value.slice(0, maxlength);
-    }
-}
+  if (input.value.length > maxlength) {
+    input.value = input.value.slice(0, maxlength);
+  }
+};
 
 const modifyUserInput = (input, maxlength) => {
-    if(input.value.length !== maxlength && input.value > 0) {
-        input.value = "0".repeat(maxlength - input.value.length) + input.value
-    }
-}
+  if (input.value.length !== maxlength && input.value > 0) {
+    input.value = "0".repeat(maxlength - input.value.length) + input.value;
+  }
+};
 
-Object.values(inputElements).forEach(input => {
-    const maxlength = input.getAttribute("maxlength");
-    input.addEventListener("input", (event) => {
-        modifyInputLength(event.target, maxlength);
-    })
+// Input Validation
+const inputValidation = (input, max, min) => {
+  if (input.value === "" || input.value === null || input.value === 0) {
+  }
+};
 
-    input.addEventListener("blur", (event) => {
-       modifyUserInput(event.target, maxlength);
-    })
-})
+Object.values(inputElements).forEach((elemenObj) => {
+  const maxlength = elemenObj.maxlength;
+  const getInputElement = elemenObj.getElement;
 
+  getInputElement.addEventListener("input", (event) => {
+    modifyInputLength(event.target, maxlength);
+  });
 
-const inputValidation = {
-    isLeapYear : (day, month, year) => {
-        if(getCalendarDay(month, year) === day) {
-            return true;
-        }
-    }   
-}
+  getInputElement.addEventListener("blur", (event) => {
+    modifyUserInput(event.target, maxlength);
+  });
+});
+
+// Submit Area
+
+const currentDate = new Date();
 
 const submit = document.querySelector("button");
 
 submit.addEventListener("click", (event) => {
-    try {
-        const getUserInputValue = new Date (
-        parseInt(inputElements["userInputYear"].value), 
-        parseInt(inputElements["userInputMonth"].value), 
-        parseInt(inputElements["userInputDay"].value)
-        )
-        alert(getUserInputValue);
-    } catch (error) {
-        alert(error);
-    }
-    })
-
+  inputElements.inputDay.getElement.style.borderColor = "red";
+});
