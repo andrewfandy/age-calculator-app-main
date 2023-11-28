afbd357b9589e117cf573b1d4bc419b2d8ac49b2
unknown <andrewfandy13@gmail.com>
2023-11-29 01:17:53 +0700

    Changes some lines

--- app.js
+++ app.js
@@ -1,146 +1,189 @@
-const currentDate = new Date();
-
-// ADD ERROR KEY
-const inputElements = {
+const inputElementList = {
    inputDay: {
-      name: "inputDay",
-      getElement: document.getElementById("inputDay"),
+      element: document.getElementById("inputDay"),
       maxlength: 2,
       min: 1,
       max: 31,
       errorMsg: {
          emptyError: "This field is required",
-         invalidError: "Must be a valid day",
-         errorDate: "Must be a valid date",
+         validError: "Must be a valid day",
+         exceedDate: "Must be in a past",
+         dateError: "Must be a valid date",
       },
    },
    inputMonth: {
-      name: "inputMonth",
-      getElement: document.getElementById("inputMonth"),
+      element: document.getElementById("inputMonth"),
       maxlength: 2,
       min: 1,
       max: 12,
       errorMsg: {
          emptyError: "This field is required",
-         invalidError: "Must be a valid month",
-         errorDate: "",
+         validError: "Must be a valid month",
+         exceedDate: "Must be in a past",
+         dateError: "",
       },
    },
    inputYear: {
-      name: "inputYear",
-      getElement: document.getElementById("inputYear"),
+      element: document.getElementById("inputYear"),
       maxlength: 4,
       min: 1,
-      max: currentDate.getFullYear(),
+      max: new Date().getFullYear(),
       errorMsg: {
          emptyError: "This field is required",
-         invalidError: "Must be in a past",
-         errorDate: "",
+         validError: "Must be in a past",
+         exceedDate: "Must be in a past",
+         dateError: "",
       },
    },
 };
 
-// Input Modification
-const modifyInputLength = (input, maxlength) => {
+const modifyInputLength = (input) => {
+   const maxlength = inputElementList[input.id].maxlength;
    if (input.value.length > maxlength) {
       input.value = input.value.slice(0, maxlength);
    }
 };
 
-const modifyUserInput = (input, maxlength) => {
-   if (input.value.length !== maxlength && input.value > 0) {
+const modifyUserInput = (input) => {
+   const maxlength = inputElementList[input.id].maxlength;
+
+   if (input.value.length < maxlength && input.value > 0) {
       input.value = "0".repeat(maxlength - input.value.length) + input.value;
    }
 };
 
-// Input Validation Area
-const showError = (msg, wrapper) => {
-   const getInputWrapper = document.querySelector(
-      `.input-wrapper.${wrapper.name}`
-   );
+const onInputEventListeners = () => {
+   Object.values(inputElementList).forEach((inputElement) => {
+      const element = inputElement.element;
+      element.addEventListener("input", (event) => {
+         modifyInputLength(event.target);
+      });
 
-   getInputWrapper.style.borderColor = "red";
-   let displayErrorMessage = document.querySelector(
-      `.error-message.${wrapper.name}`
-   );
-
-   if (!displayErrorMessage) {
-      displayErrorMessage = document.createElement("p");
-      displayErrorMessage.classList.add("error-message", `${wrapper.name}`);
-      getInputWrapper.appendChild(displayErrorMessage);
-      displayErrorMessage.textContent = msg;
-   }
-   return false;
+      element.addEventListener("blur", (event) => {
+         modifyUserInput(event.target);
+      });
+   });
 };
 
-// Invalid Date
-const maximumDayOnMonth = () => {
-   const yearValue = inputElements.inputYear.getElement.value;
-   const monthValue = inputElements.inputMonth.getElement.value;
-   return new Date(yearValue, monthValue, 0).getDate();
+onInputEventListeners();
+
+const getCurrentDate = () => {
+   return new Date();
 };
 
-// const isEmptyValidation = (inputValue, )
-const inputValidation = (inputElementObject, inputElement) => {
-   const errorMessages = inputElementObject.errorMsg;
-   const getErrorMessage = document.querySelector(
-      `.error-message.${inputElementObject.name}`
+const setInputDate = () => {
+   const inputDate = new Date();
+   inputDate.setFullYear(
+      parseInt(inputElementList.inputYear.element.value, 10)
    );
-   const inputValue = parseInt(inputElement.value, 10);
-   let isValid = false;
+   inputDate.setMonth(
+      parseInt(inputElementList.inputMonth.element.value, 10) - 1
+   );
+   inputDate.setDate(parseInt(inputElementList.inputDay.element.value, 10));
+   return inputDate;
+};
 
-   if (!inputValue) {
-      showError(errorMessages.emptyError, inputElementObject);
+const isValidDate = () => {
+   const year = parseInt(document.getElementById("inputYear").value, 10);
+   const month = parseInt(document.getElementById("inputMonth").value, 10);
+   const day = parseInt(document.getElementById("inputDay").value, 10);
+
+   return new Date(year, month, 0).getDate() >= day;
+};
+const displayErrorMessage = (msg, inputId) => {
+   const wrapper = document.querySelector(`.input-wrapper.${inputId}`);
+
+   const getInputElement = document.querySelectorAll("input");
+   [...getInputElement].map((element) => {
+      return (element.style.borderColor = "hsl(0, 100%, 67%)");
+   });
+   const errorMessage =
+      wrapper.querySelector(".error-message") || document.createElement("p");
+   errorMessage.classList.add("error-message", `${inputId}`);
+
+   wrapper.appendChild(errorMessage);
+   errorMessage.textContent = msg;
+};
+
+const submitValidation = (input) => {
+   const getInputAttributes = inputElementList[input.id];
+   const value = parseInt(input.value, 10);
+   const { min, max, errorMsg } = getInputAttributes;
+   const getErrorMessage = document.querySelector(`.error-message.${input.id}`);
+   if (!value) {
+      displayErrorMessage(errorMsg.emptyError, input.id);
+      return false;
+   } else if (value > max || value < min) {
+      displayErrorMessage(errorMsg.validError, input.id);
+      return false;
+   } else if (!isValidDate()) {
+      displayErrorMessage(errorMsg.dateError, input.id);
+      return false;
    } else {
+      if (setInputDate() > getCurrentDate()) {
+         displayErrorMessage(errorMsg.exceedDate, input.id);
+         return false;
+      }
+
       if (getErrorMessage) {
          getErrorMessage.remove();
+         document.querySelector(
+            `.input-wrapper.${input.id} input`
+         ).style.borderColor = "";
       }
    }
-
-   if (
-      inputValue > inputElementObject.max ||
-      inputValue < inputElementObject.min
-   ) {
-      console.log("olin");
-      showError(errorMessages.invalidError, inputElementObject);
-   }
-
-   if (inputElement.id === "inputDay" && inputValue) {
-      if (inputValue !== maximumDayOnMonth()) {
-         showError(errorMessages.errorDate, inputElementObject);
-      } else {
-         console.log("test");
-      }
-   }
-   return isValid;
+   return true;
 };
 
-Object.values(inputElements).forEach((inputElementObject) => {
-   const maxlength = inputElementObject.maxlength;
-   const getInputElement = inputElementObject.getElement;
+const displayResult = () => {
+   const inputDate = setInputDate();
+   const currDate = getCurrentDate();
 
-   getInputElement.addEventListener("input", (event) => {
-      modifyInputLength(event.target, maxlength);
-   });
+   let showYear = currDate.getFullYear() - inputDate.getFullYear();
+   let showMonth = currDate.getMonth() - inputDate.getMonth();
+   let showDay = currDate.getDate() - inputDate.getDate();
 
-   getInputElement.addEventListener("blur", (event) => {
-      modifyUserInput(event.target, maxlength);
-   });
-});
-
-// Submit Area
-
-const submit = document.querySelector("button");
-
-submit.addEventListener("click", (event) => {
-   event.preventDefault();
-   let isValid = true;
-   Object.values(inputElements).forEach((inputElementObject) => {
-      const getInputElement = inputElementObject.getElement;
-      isValid = inputValidation(inputElementObject, getInputElement);
-   });
-
-   if (isValid) {
-      console.log(true + " all");
+   if (showDay < 0) {
+      showMonth -= 1;
+      showDay += new Date(
+         currDate.getFullYear(),
+         currDate.getMonth(),
+         0
+      ).getDate();
    }
-});
+
+   if (showMonth < 0) {
+      showYear -= 1;
+      showMonth += 12;
+   }
+
+   const getYearOutput = document.getElementById("showYear");
+   const getMonthOutput = document.getElementById("showMonth");
+   const getDayOutput = document.getElementById("showDay");
+
+   getYearOutput.textContent = showYear;
+   getMonthOutput.textContent = showMonth;
+   getDayOutput.textContent = showDay;
+};
+
+const onSubmitEventListeners = () => {
+   const form = document.querySelector(".form-input-wrapper");
+
+   form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      let isFormValid = true;
+
+      Object.values(inputElementList).forEach((inputElement) => {
+         const element = inputElement.element;
+         if (!submitValidation(element)) {
+            isFormValid = false;
+         }
+      });
+
+      if (isFormValid) {
+         displayResult();
+      }
+   });
+};
+
+onSubmitEventListeners();
